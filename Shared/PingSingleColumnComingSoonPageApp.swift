3707d5ffd47a86aecd430c7675e33976234745be
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-04 08:10:00 +0900

    Initial Commit

---
+++ Shared/PingSingleColumnComingSoonPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingSingleColumnComingSoonPageApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/04/04.
+//
+
+import SwiftUI
+
+@main
+struct PingSingleColumnComingSoonPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
