52dcea399055f90f3e01ca05aa0e1213d2282d2f
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-04 08:50:24 +0900

    Add the email validation functionalities.

---
+++ Shared/Views/SocialMediaLinks.swift
@@ -0,0 +1,22 @@
+//
+//  SocialMediaLinks.swift
+//  PingSingleColumnComingSoonPage (iOS)
+//
+//  Created by Jinwook Kim on 2022/04/04.
+//
+
+import SwiftUI
+
+struct SocialMediaLinks: View {
+    var body: some View {
+        HStack {
+            
+        }
+    }
+}
+
+struct SocialMediaLinks_Previews: PreviewProvider {
+    static var previews: some View {
+        SocialMediaLinks()
+    }
+}
--- Shared/Views/SubscriptionForm.swift
+++ Shared/Views/SubscriptionForm.swift
@@ -9,18 +9,41 @@ import SwiftUI
 
 struct SubscriptionForm: View {
     @State private var emailAddress: String = ""
+    @State private var isEmailAddressInvalid: Bool = false
     var body: some View {
-        VStack {
+        VStack(spacing: 5) {
             TextField("Your email address...", text: $emailAddress)
                 .textFieldStyle(CustomTextFieldStyle())
+                .overlay(
+                    Capsule()
+                        .stroke(.red, lineWidth: 1)
+                        .opacity(isEmailAddressInvalid ? 1 : .zero)
+                )
                 .keyboardType(.emailAddress)
+                .textInputAutocapitalization(.never)
+                .onSubmit {
+                    validateEmailAddress()
+                }
+            Text("Please provide a valid email address")
+                .font(.caption)
+                .fontWeight(.light)
+                .foregroundColor(.red)
+                .italic()
+                .opacity(isEmailAddressInvalid ? 1 : .zero)
             Button {
-
+                validateEmailAddress()
             } label: {
                 NotificationButton()
             }
         }
         .scaledToFit()
+    }
+    func validateEmailAddress() -> Void {
+        let emailRegex: String = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate: NSPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        withAnimation(.easeInOut(duration: 0.3)) {
+            isEmailAddressInvalid = !emailPredicate.evaluate(with: emailAddress)
+        }
     }
 }
 
